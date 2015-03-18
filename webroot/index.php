f8b06f078af4bef3a04b84c7bd4c0eacfc4f57a2
Félix Bouynot <felixbouynot@gmail.com>
2015-03-18 17:56:01 +0100

    Change login against cd2 w/ lower charge

--- webroot/index.php
+++ webroot/index.php
@@ -10,7 +10,7 @@ if(!(isset($_SESSION["connected"]) && ($_SESSION["connected"] == true)))
     {
         $dn = "OU=RH,DC=oiio,DC=loc";
         $filter = "(cn=*)";
-        $ad = ldap_connect("ldap://cd1.oiio.loc") or die("Couldn't connect to AD!");
+        $ad = ldap_connect("ldap://cd2.oiio.loc") or die("Couldn't connect to AD!");
         ldap_set_option($ad, LDAP_OPT_PROTOCOL_VERSION, 3);
         ldap_set_option($ad, LDAP_OPT_REFERRALS, 0);
         if (@$bd = ldap_bind($ad, $_POST['username'] . "@oiio.loc", $_POST['userpwd']))
