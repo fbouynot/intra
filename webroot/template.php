cd80e941b9a8c45dd559390218f087331ff77057
Félix Bouynot <felixbouynot@gmail.com>
2015-03-18 14:33:51 +0100

    template

---
+++ webroot/template.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang='fr'>
+	<head>
+		<meta charset='utf-8' name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<title>OIIO Formation - Intranet</title>
+	</head>
+    <body>
+        <!-- Header -->
+        <header>
+        </header>
+        
+        <?php
+            echo $content;
+        ?>
+        
+        <!-- Footer -->
+        <footer>
+        </footer>
+    </body>
+    <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+</html>
