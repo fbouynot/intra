3d62531a6c12b72321f2e72b3411964c68ec3220
Félix Bouynot <felixbouynot@gmail.com>
2015-03-18 14:24:47 +0100

    mvc

---
+++ webroot/index.php
@@ -0,0 +1,25 @@
+<?php
+
+define('WEBROOT',dirname(__FILE__));
+define('ROOT',dirname(WEBROOT));
+define('BASE_URL',dirname(dirname($_SERVER['SCRIPT_NAME'])));
+
+if(!isset($_GET['p']) || $_GET['p'] == "")
+{
+    $_GET['p'] = 'index';
+}
+
+if(strpos($_GET['p'],"/") != false)
+{
+    $params = explode("/",$_GET['p']);
+    $_GET['p'] = $params[1];
+}
+
+if(!file_exists(ROOT . "/controler/".$_GET['p'].".php"))
+{
+    $_GET['p'] = '404';
+}
+ob_start(); // Suspend l'affichage
+include ROOT . "/controler/".$_GET['p'].".php";
+$content = ob_get_clean(); // Récupération du contenu
+include(WEBROOT . "/template.php");
