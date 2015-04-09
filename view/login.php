6c45afc996cdfc157829cf5b38e1309e73e81dcd
Agadagluglu <alex.punch@netcourrier.com>
2015-04-09 12:15:44 +0200

    Tests on the login page
    <button> seems to work fine.

--- view/login.php
+++ view/login.php
@@ -1,10 +1,15 @@
 <!-- Formulaire de connexion -->
-<form method="post" action="<?php echo BASE_URL;?>/login" enctype="multipart/form-data">
-        <label for="userName">Nom d'utilisateur</label>
-        <input id="userName" name="userName">
+<h3>Se connecter</h3>
+<div class="login">
+    <form method="post" action="<?php echo BASE_URL;?>/login" enctype="multipart/form-data">
+            <label for="userName">Nom d'utilisateur</label>
+            <input id="userName" name="userName">
 
-        <label for="userPwd">Mot de passe</label>
-        <input id="userPwd" name="userPwd" type="password">
+            <label for="userPwd">Mot de passe</label>
+            <input id="userPwd" name="userPwd" type="password">
 
-        <input type="submit" value="Se connecter">
-</form>
+            <!--<input type="submit" value="Se connecter">-->
+            <button type="submit">Se connecter</button>
+    </form>    
+<a href="#" >Identifiants oubliés ?</a>
+</div>
