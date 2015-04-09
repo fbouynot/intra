574aba4735e910a28e6a93b6184092647003dd0c
Félix Bouynot <felixbouynot@gmail.com>
2015-04-09 14:16:31 +0200

    Change login to first name

--- core/ldap_authentication.php
+++ core/ldap_authentication.php
@@ -8,7 +8,9 @@ if(!(isset($_SESSION["connected"]) && ($_SESSION["connected"] == true)))
         /* La raçine de notre recherche */
         $dn = "OU=RH,DC=oiio,DC=loc";
         /* Ce que l'on cherche */
-        $filter = "(cn=*)";
+        $filter = "(sAMAccountName=" . $_POST['userName'] . ")";
+        /* Les attributs recherchés : mail, prenom, nom de famille, login */
+        $attr = array("mail", "sn", "givenname", "samaccountname");
         /* Contact de l'AD */
         $ad = ldap_connect("ldap://cd2.oiio.loc") or die("Couldn't connect to AD!");
         ldap_set_option($ad, LDAP_OPT_PROTOCOL_VERSION, 3);
@@ -16,6 +18,12 @@ if(!(isset($_SESSION["connected"]) && ($_SESSION["connected"] == true)))
         /* Connexion à l'AD avec les identifiants de l'utilisateur. Si c'est accepté, on connecte l'utilsateur. */
         if (@$bd = ldap_bind($ad, $_POST['userName'] . "@oiio.loc", $_POST['userPwd']))
         {
+            $result = ldap_search($ad,$dn,$filter,$attr) or die ("Erreur de recherche  : " . ldap_error($ad));
+            $result = ldap_get_entries($ad, $result);
+            
+            @$_SESSION['sn'] = $result[0]['sn'][0];
+            $_SESSION['givenName'] = $result[0]['givenname'][0];
+            @$_SESSION['mail'] = $result[0]['mail'][0];
             $_SESSION["connected"] = true;
             $_SESSION["userName"] = $_POST["userName"];
             $_SESSION["userPwd"] = $_POST["userPwd"];
