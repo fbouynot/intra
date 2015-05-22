840c629cde7c65d6cf6460d8cc2140994782e401
Agadagluglu <alex.punch@netcourrier.com>
2015-05-22 16:25:10 +0200

    Loing button test

--- view/cv.php
+++ view/cv.php
@@ -3,7 +3,9 @@
         <form method="post" action="<?php echo BASE_URL . '/cv';?>" enctype="multipart/form-data">
             <label for="cv"><h3>Votre CV</h3></label>
             <input type="hidden" name="MAX_FILE_SIZE" value="10000000">
-            <input type="file" name="cv" />
+            <button id="button-input-file">
+                <input id="input-file" type="file" name="cv" />
+            </button>
             <button class="button-apply" type="submit" name="submit">Envoyer</button>
         </form>
     </div>    
