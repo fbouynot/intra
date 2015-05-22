377e701ee02825dfacbc5ec27c6e29c040272ef4
Agadagluglu <alex.punch@netcourrier.com>
2015-05-22 15:19:02 +0200

    Update CV form display

--- view/cv.php
+++ view/cv.php
@@ -1,11 +1,10 @@
-<div class="center">    
-    <form method="post" action="<?php echo BASE_URL . '/cv';?>" enctype="multipart/form-data">
-
-        <label for="cv">Votre CV</label>
-        <input type="hidden" name="MAX_FILE_SIZE" value="10000000">
-
-        <input type="file" name="cv" />
-        <button type="submit" name="submit">Envoyer</button>
-
-    </form>
+<div class="center">
+    <div class="wrapper">
+        <form method="post" action="<?php echo BASE_URL . '/cv';?>" enctype="multipart/form-data">
+            <label for="cv"><h3>Votre CV</h3></label>
+            <input type="hidden" name="MAX_FILE_SIZE" value="10000000">
+            <input type="file" name="cv" />
+            <button class="button-apply" type="submit" name="submit">Envoyer</button>
+        </form>
+    </div>    
 </div>
