d50660b1bc2334460aef01269374aacec77aef9d
Félix Bouynot <felixbouynot@gmail.com>
2015-05-27 17:07:33 +0200

    nginx path

--- view/cv.php
+++ view/cv.php
@@ -13,7 +13,7 @@ else
     ?>
 <div class="center">
     <div class="wrapper">
-        <form method="post" action="<?php echo BASE_URL . '/cv';?>" enctype="multipart/form-data">
+        <form method="post" action="<?php echo BASE_URL . 'cv';?>" enctype="multipart/form-data">
             <h3>Votre CV</h3>
             <label class="custom-input" for="input-file">
                 <input type="hidden" name="MAX_FILE_SIZE" value="10000000">
